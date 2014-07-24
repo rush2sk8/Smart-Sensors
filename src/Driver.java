b972823f53ab85d17c3ba7b6d8978d6cf5acea2f
rush2sk8 <rushadantia@gmail.com>
2014-07-24 10:58:50 -0400

    Shows NIC's
    Show NIC's

--- src/Driver.java
+++ src/Driver.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
 
 
 public class  Driver{
@@ -7,10 +9,14 @@ public class  Driver{
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		NCAP ncap = new NCAP("http://192.168.254.102", 10); 
+		Enumeration<NetworkInterface> interfaces = null;
+		interfaces = NetworkInterface.getNetworkInterfaces();
+		while(interfaces.hasMoreElements()) {
+			NetworkInterface nic = interfaces.nextElement();
+			if(nic.isUp())
+				System.out.println("Interface Name: "+nic.getDisplayName());
 
-		System.out.println(ncap.queryTEDS(106, 100, 1, 10));
-
-
+		}
 	}
 
 }
