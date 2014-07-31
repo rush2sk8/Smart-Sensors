d83493d117a918dd105a3573e45a92f65584f2df
rush2sk8 <rushadantia@gmail.com>
2014-07-31 14:58:06 -0400

    idek

--- src/Driver.java
+++ src/Driver.java
@@ -1,11 +1,21 @@
+import java.io.IOException;
+
 public class  Driver{
 
 
 
-	public static void main(String[] args) {
- 
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		///NCAP ncap = new NCAP("http://192.168.254.102", 10);
 
-		 
+		for(int i=0;i<50;i++) {
+
+			//String datac = ncap.getSensorDataRaw(107, 1, 10);
+			//double data =Double.parseDouble(datac.substring(0,datac.length()-2));
+
+			System.out.println();
+
+		}
+
 	}
 
 }
